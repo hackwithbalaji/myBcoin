a0e62e79f61aeb234433a9d054e702d674554adc
Balaji <hackwithbalaji@gmail.com>
2022-01-02 00:13:46 +0530

    Added docstrings

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import json;
 print("Hey, its under development...")
 
 class Block:
+    """
+    Represents a single block
+    """
     def __init__(self,index, data, previousHash = None):
         self.index = index
         self.createdOn = str(datetime.now())
@@ -18,18 +21,38 @@ class Block:
         ).hexdigest()
 
 class BlockChain:
+    """
+    Represents the Blockchain.
+    """
     def __init__(self):
         self.myBChain = [self.createGenesisBlock()]
 
     def createGenesisBlock(self):
+        """
+        Creates a genesis block(first block of the BlockChain)
+
+        Returns: a block
+        """
         return Block( 0, { "price" : 100})
 
     def addNewBlock(self, data):
-        return self.myBChain.append(
+        """
+        Adds a new block to the chain
+
+        Args: 
+            data - data to be added in the block
+        """
+        self.myBChain.append(
             Block(len(self.myBChain), data, self.myBChain[len(self.myBChain) - 1].hash)
         )
 
     def isChainValid(self):    
+        """
+        Check for chain validity
+
+        Returns:
+            bool: true/false 
+        """
         for i in range(1, len(self.myBChain)):
             if self.myBChain[i].previousHash != self.myBChain[i-1].hash:
                 return False
